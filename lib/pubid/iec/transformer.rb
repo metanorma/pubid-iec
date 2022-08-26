a6e08c2501973c0362d0995308861cf03b215e5b
Artur Komarov <artur.komarov@gmail.com>
2022-08-26 14:07:43 +0700

    Update transformer to latest pubid-core changes

--- lib/pubid/iec/transformer.rb
+++ lib/pubid/iec/transformer.rb
@@ -1,7 +1,21 @@
 module Pubid::Iec
-  class Transformer < Parslet::Transform
-    rule(database: "DB") do
-      { database: true }
+  class Transformer < Pubid::Core::Transformer
+    class << self
+      def get_amendment_class
+        Amendment
+      end
+
+      def get_corrigendum_class
+        Corrigendum
+      end
+    end
+
+    def initialize
+      super
+
+      rule(database: "DB") do
+        { database: true }
+      end
     end
   end
 end
